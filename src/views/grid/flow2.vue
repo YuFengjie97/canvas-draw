35a801b8036e06b9f5190a588da65785b80dac66
yufengjie <2457522528@qq.com>
2023-10-09 16:34:56 +0800

    flow2

--- src/views/grid/flow2.vue
+++ src/views/grid/flow2.vue
@@ -2,35 +2,99 @@
 import { onMounted, ref } from 'vue'
 import Stats from 'stats.js'
 import { Vector } from 'p5'
+import * as dat from 'dat.gui'
 
 import { Noise } from 'noisejs'
-import { initCanvas, map } from '@/utils'
+import { map } from '@/utils'
 
 const con = ref<HTMLElement>()
+const canvas = ref<HTMLCanvasElement>()
 const stats = new Stats()
 document.body.appendChild(stats.dom)
+let hueBase = 300
+let hueRange = 360
+let lineWidth = 10
+let cellSize = 30
+let lineLen = lineWidth * 10
+let width = 0
+let height = 0
+let ctx: CanvasRenderingContext2D
+let rows = 0
+let cols = 0
+let isSimplex = true
+let xInc = 0.001
+let yInc = 0.001
+let zInc = 0.01
 
-const hueRange = [0, 340]
+const ui = {
+  hueBase,
+  hueRange,
+  lineWidth,
+  cellSize,
+  lineLen,
+  isSimplex,
+  xInc,
+  yInc,
+  zInc,
+}
 
 onMounted(() => {
-  const conEl = con.value as HTMLElement
-  const { width, height } = conEl.getBoundingClientRect()
-  const ctx = initCanvas(conEl, { width, height })
+  function init() {
+    const { width: w, height: h } = con.value!.getBoundingClientRect()
+    width = w
+    height = h
 
+    canvas.value!.width = w
+    canvas.value!.height = h
+    ctx = canvas.value!.getContext('2d')!
+    rows = Math.ceil(h / cellSize)
+    cols = Math.ceil(w / cellSize)
+
+    ctx.lineCap = 'round'
+    ctx.lineWidth = lineWidth
+  }
+  init()
+
+  const cells: Cell[] = []
   const noise = new Noise(Math.floor(Math.random() * 10000))
 
-  const lineWidth = 10
-  const cellSize = 30
-  const lineLen = lineWidth * 10
-  const rows = Math.ceil(height / cellSize)
-  const cols = Math.ceil(width / cellSize)
-  const cells: Cell[] = []
+  function windowResize() {
+    let timer: null | NodeJS.Timeout = null
+    return () => {
+      if (timer)
+        clearTimeout(timer)
 
-  ctx.lineCap = 'round'
-  ctx.lineWidth = lineWidth
+      timer = setTimeout(() => {
+        init()
+        cells.length = 0
+        initCells()
+      }, 700)
+    }
+  }
+  window.addEventListener('resize', windowResize())
 
-  let min = 0
-  let max = 0
+  function initUi() {
+    const panel = new dat.GUI({ width: 300 })
+    panel.add(ui, 'hueBase', 1, 360, 1).onChange(val => hueBase = val)
+    panel.add(ui, 'hueRange', 1, 360, 1).onChange(val => hueRange = val)
+    panel.add(ui, 'isSimplex').name('Simplex3/Perlin3').onChange(val => isSimplex = val)
+    panel.add(ui, 'cellSize', 10, 60, 1).onChange((val) => {
+      cellSize = val
+      cells.length = 0
+      rows = Math.ceil(height / cellSize)
+      cols = Math.ceil(width / cellSize)
+      initCells()
+    })
+    panel.add(ui, 'lineWidth', 1, 20, 1).onChange((val) => {
+      lineWidth = val
+      ctx.lineWidth = lineWidth
+    })
+    panel.add(ui, 'lineLen', 1, 200, 1).onChange(val => lineLen = val)
+    panel.add(ui, 'xInc', 0.001, 0.01, 0.001).onChange(val => xInc = val)
+    panel.add(ui, 'yInc', 0.001, 0.01, 0.001).onChange(val => yInc = val)
+    panel.add(ui, 'zInc', 0.01, 0.1, 0.01).onChange(val => zInc = val)
+  }
+  initUi()
 
   class Cell {
     end = new Vector(0, 0)
@@ -55,10 +119,7 @@ onMounted(() => {
     }
 
     getColor() {
-      this.hue = Math.floor(map(this.getNoiseXYZ(), -1, 1, hueRange[0], hueRange[1]))
-
-      min = this.hue < min ? this.hue : min
-      max = this.hue > max ? this.hue : max
+      this.hue = Math.floor(map(this.getNoiseXYZ(), -1, 1, hueBase - hueRange, hueBase + hueRange))
 
       const grad = ctx.createLinearGradient(this.pos.x, this.pos.y, this.end.x, this.end.y)
       grad.addColorStop(0, `hsla(${this.hue}, 100%, 50%, 0)`)
@@ -67,7 +128,10 @@ onMounted(() => {
     }
 
     getNoiseXYZ() {
-      return noise.simplex3(this.xOff, this.yOff, this.zOff)
+      if (isSimplex)
+        return noise.simplex3(this.xOff, this.yOff, this.zOff)
+
+      return noise.perlin3(this.xOff, this.yOff, this.zOff)
     }
 
     update() {
@@ -76,9 +140,9 @@ onMounted(() => {
       const len = map(n, -1, 1, 0.1, lineLen)
 
       this.end = this.pos.copy().add(Vector.fromAngle(a, len))
-      this.xOff += 0.001
-      this.yOff += 0.001
-      this.zOff += 0.01
+      this.xOff += xInc
+      this.yOff += yInc
+      this.zOff += zInc
     }
   }
 
@@ -109,11 +173,10 @@ onMounted(() => {
 </script>
 
 <template>
-  <div ref="con" class="h-100vh w-full" />
+  <div ref="con" class="h-100vh w-full">
+    <canvas ref="canvas" class="h-full w-full block" />
+  </div>
 </template>
 
 <style lang='less' scoped>
-.view-con{
-  color: hsl(96, 100%, 50%);
-}
 </style>
